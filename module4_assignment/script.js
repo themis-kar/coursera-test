5c20ac5676f9fb1dfa1ac98b675637bd4a3dd535
themis-kar <themis90kar@gmail.com>
2016-07-16 14:35:19 +0300

    Module 4 solution

---
+++ module4_assignment/script.js
@@ -0,0 +1,16 @@
+(function () {
+
+var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
+
+for (var i = 0; i < names.length; i++) {
+
+	var firstLetter = names[i].charAt(0);
+	firstLetter = firstLetter.toLowerCase();
+
+	if (firstLetter == "j") {
+		byeSpeaker.speak(names[i]);
+	} else {
+		helloSpeaker.speak(names[i]);
+	}
+}
+})();
